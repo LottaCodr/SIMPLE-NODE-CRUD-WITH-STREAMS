be4ecb9c2f9fc62a3cfd7cea54646f17c0e3a711
King-Lota <lottanna47@gmail.com>
2023-11-21 01:22:34 +0100

    Read & Write Streams Added

--- streams.js
+++ streams.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 
-const readStream = fs.createReadStream('./docs/streamtester.txt');
+const readStream = fs.createReadStream('./docs/streamtester.txt',{encoding: 'utf8'});
+const writeStream = fs.createWriteStream('./docs/streamtester2.txt', {encoding: 'utf8'})
 
 readStream.on('data', (pile)=> {
 console.log('New Pile of data')
-    console.log(pile.toString());
+console.log(pile);
+
+writeStream.write('\nNew Pile Below \n');
+writeStream.write(pile);
 });
